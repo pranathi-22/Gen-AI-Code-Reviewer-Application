34a985fff9c4710fe296409e0b77f2fd8bed1a01
pranathi-22 <161023768+pranathi-22@users.noreply.github.com>
2024-04-16 18:31:04 +0530

    Update app.py

--- OpenAI/app.py
+++ OpenAI/app.py
@@ -7,15 +7,15 @@ OPENAI_API_KEY = "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"
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 # Set colored page title
-st.markdown("<h1 style='color:black;'>GenAI App - AI Code Reviewer</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color:purple;'>GenAI App - AI Code Reviewer</h1>", unsafe_allow_html=True)
 
 # User input section
-st.markdown("<h2 style='color:black;'>Enter Your Python Code</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='color:purple;'>Enter Your Python Code</h2>", unsafe_allow_html=True)
 prompt = st.text_area("Enter your Python code here:", height=200)
 
 # Button to trigger code review
 if st.button("Review the Code"):
-    st.markdown("<h2 style='color:black;'>Code Reviewer</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='color:green;'>Code Reviewer</h2>", unsafe_allow_html=True)
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-0301",
         messages=[
